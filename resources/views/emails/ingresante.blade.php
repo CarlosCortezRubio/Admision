ec4ccf584b19a4394ed02a001f70f70f146fe067
CabanillasAlvaro <ctorresalvar@crece.uss.edu.pe>
2021-10-17 17:36:23 -0500

    ultima actulializacion de pago de derecho

--- resources/views/emails/ingresante.blade.php
+++ resources/views/emails/ingresante.blade.php
@@ -6,8 +6,8 @@
 
 @section('content')
    <p>Usted a logrado pasar satisfactoriamente la etapa de evaluación de ingreso</p>
-   <p>Para culminar con el proceso de inscripción pulse en el botón "Continuar":</p>
-   @include('partials.email-action-button', ['link'=>$enlace, 'label'=>'Continuar'])
+   <p>Para culminar con el proceso de inscripción pulse en el botón "Pagar derecho de Ingreso":</p>
+   @include('partials.email-action-button', ['link'=>$enlace, 'label'=>'Pagar derecho de Ingreso'])
 
 @endsection
 
