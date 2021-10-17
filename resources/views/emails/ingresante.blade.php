53b9aa21bdf1ee0087bd5b1b700100c14540b994
CabanillasAlvaro <ctorresalvar@crece.uss.edu.pe>
2021-10-17 16:40:52 -0500

    confirmación de correo de pasante

--- resources/views/emails/ingresante.blade.php
+++ resources/views/emails/ingresante.blade.php
@@ -5,9 +5,9 @@
 @endsection
 
 @section('content')
-   <p>usted a logrado pasar satisfactoriamente la etapa de evaluación de ingreso:</p>
+   <p>Usted a logrado pasar satisfactoriamente la etapa de evaluación de ingreso, para culminar con el proceso de inscripción:</p>
 
-   @include('partials.email-action-button', ['link'=>$enlace, 'label'=>'Para culminar con el proceso de inscripción, ingrese aquí'])
+   @include('partials.email-action-button', ['link'=>$enlace, 'label'=>'Ingrese aquí'])
 
    <p>Este enlace estará disponible por 60 minutos desde su recepción.</p>
 @endsection
