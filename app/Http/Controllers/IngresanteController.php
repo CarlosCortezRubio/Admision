1ef0a6610a008e07644da570c43fcedcfcdb7a9f
CarlosCortezRubio <presto_ccr@hotmail.com>
2021-11-15 14:26:24 -0500

    -

--- app/Http/Controllers/IngresanteController.php
+++ app/Http/Controllers/IngresanteController.php
@@ -10,6 +10,6 @@ class IngresanteController extends Controller
 {
     public function SimularIngresante(){
         Mail::to(config('app.email_soporte'))->send(new MensajeIngresantes(config('app.url_pasarela').'/simularPago'));
-        return 'mensaje simulado con exito';
+        return redirect()->back();
     }
 }
