e1b1208ca75fce28ce3d31abfbb86f3c1cdb5917
CarlosCortezRubio <presto_ccr@hotmail.com>
2021-12-07 23:06:43 -0500

    ---

--- resources/views/inscripcion/ficha/edit.blade.php
+++ resources/views/inscripcion/ficha/edit.blade.php
@@ -406,6 +406,7 @@
       <div class="row">
          <div class="col-12">
             <!-- 2021 -->
+            {{--@include('inscripcion.ficha.edit-repertory')--}}                       
             <div class="card card-primary card-outline elevation-2">
                <div class="card-header"><strong>T&eacute;cnica: Escalas y estudios</strong></div>
                <div class="card-body">
