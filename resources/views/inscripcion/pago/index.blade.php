63358a783858bfdf4b40385f4b2153815c7f8a96
CarlosCortezRubio <presto_ccr@hotmail.com>
2021-12-12 16:50:25 -0500

    --

--- resources/views/inscripcion/pago/index.blade.php
+++ resources/views/inscripcion/pago/index.blade.php
@@ -214,11 +214,11 @@
                format: 'DD/MM/YYYY'
             }
          }, function(start, end, label) {
-            var years = moment().diff(start, 'years');
-
+            var fcalc = moment();
+            var edad = fcalc.diff(start, 'years');
             $('#nacimiento').val(start.format('DD/MM/YYYY'));
             $('#fech_naci_pos').val(start.format('YYYY-MM-DD'));
-            $('#edad_calc_pos').val(years);
+            $('#edad_calc_pos').val(edad);
 
          // Filtrar especialidades
          $('#s_seccion').change(function() {
