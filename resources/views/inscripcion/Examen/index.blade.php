d4230fc4249b91587c320e2ab59a689f495b369e
CarlosCortezRubio <presto_ccr@hotmail.com>
2022-01-03 12:56:39 -0500

    --

--- resources/views/inscripcion/Examen/index.blade.php
+++ resources/views/inscripcion/Examen/index.blade.php
@@ -75,8 +75,8 @@
         /*$('body').on("contextmenu",function(e){
             return false;
         });*/
-        $('body').bind('cut copy paste', function(e){
+       /* $('body').bind('cut copy paste', function(e){
             e.preventDefault();
-        });
+        });*/
     </script>
 @endsection
