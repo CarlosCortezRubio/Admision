203f2faa1896cec0a13643d943254f270d33578c
CarlosCortezRubio <presto_ccr@hotmail.com>
2022-01-03 11:56:49 -0500

    --

--- app/Http/Controllers/ExamenController.php
+++ app/Http/Controllers/ExamenController.php
@@ -12,15 +12,15 @@ class ExamenController extends Controller
 {
     public function index(Request $request){
         $examen=DB::table('admision.adm_examen_postulante as ep')
-                ->join('admision.adm_postulante as ps','ps.id_postulante','ep.id_postulante')
-                ->join('admision.adm_programacion_examen as pe','pe.id_programacion_examen','ps.id_programacion_examen')
-                ->join('admision.adm_examen as e','pe.id_examen','e.id_examen')
-                ->where('ps.nume_docu_sol',Auth::user()->ndocument)
-                ->where('pe.id_examen',$request->id_examen)
-                ->select('e.enlace',
-                         'ep.id_examen_postulante',
-                         "ep.minutos",
-                         "ep.segundos")
+               // ->join('admision.adm_postulante as ps','ps.id_postulante','ep.id_postulante')
+               // ->join('admision.adm_programacion_examen as pe','pe.id_programacion_examen','ps.id_programacion_examen')
+               // ->join('admision.adm_examen as e','pe.id_examen','e.id_examen')
+               // ->where('ps.nume_docu_sol',Auth::user()->ndocument)
+               // ->where('pe.id_examen',$request->id_examen)
+               // ->select('e.enlace',
+               //          'ep.id_examen_postulante',
+               //          "ep.minutos",
+               //          "ep.segundos")
                 ->first();
         return $examen;
         session(['examen'=>$examen->enlace,'minutos'=>$examen->minutos,'segundos'=>$examen->segundos,'id_examen_postulante'=>$examen->id_examen_postulante]);
