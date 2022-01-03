432308d9f9e55258701ab9fd3ceefdd20e156e07
CarlosCortezRubio <presto_ccr@hotmail.com>
2022-01-03 08:33:40 -0500

    --

--- resources/views/inscripcion/ficha/horario.blade.php
+++ resources/views/inscripcion/ficha/horario.blade.php
@@ -75,15 +75,12 @@
                                 </div>
                             </div>
                         </div>
-                        <h1>{{ $v->flag_jura }}</h1>
-                        <h1>{{ $v->fecha_comp }}</h1>
-                        <h1>{{ Carbon\Carbon::now() }}</h1>
-                        <h1>{{  $v->estado }}</h1>
+                        
                        @if ($v->flag_jura=='N' && $v->fecha_comp<=Carbon\Carbon::now() && $v->estado=='P')
                        <form action="{{ route('BeginExamen') }}" method="POST">
                             <input type="text" class="d-none" name='id_examen' value="{{ $v->id_examen }}"> 
                             <div style="padding-bottom: 20px" class='centrar-content'>
-                                <a type="submit" class="btn btn-success">Ingresar</a>
+                                <button type="submit" class="btn btn-success">Ingresar</button>
                             </div>
                         </form>
                        @endif
