549e9f31cb613b3468afc9ea4cba7e3552dda6e2
CarlosCortezRubio <presto_ccr@hotmail.com>
2022-01-04 07:47:47 -0500

    --

--- resources/views/inscripcion/ficha/horario.blade.php
+++ resources/views/inscripcion/ficha/horario.blade.php
@@ -1,49 +1,50 @@
 <div class='container'>
     <div class='row'>
         @if (!$horarios)
-        <div class="container justify-content-center align-items-center">
-            <div class="card card-primary card-outline elevation-2">
-               <div class="card-body">
-                  <p>No existe horario disponible para este periodo</p>
-               </div>
-            </div>
-         </div>
-        @else
-        <div class="container">
-            
-        
-            @foreach ($horarios as $k => $v)
-                <div class="col card card-primary card-outline elevation-2">
-                    <div id='title'>
-                        <hr width="100%" size="5" noshade>
-                        <h5 class="centrar-content">{{ $v->descripcion }}</h5>
-                        <hr width="100%" size="5" noshade>
+            <div class="container justify-content-center align-items-center">
+                <div class="card card-primary card-outline elevation-2">
+                    <div class="card-body">
+                        <p>No existe horario disponible para este periodo</p>
                     </div>
-                    <div class='container'>
-                        <div class='row'>
-                            <div class='col'>
+                </div>
+            </div>
+        @else
+            <div class="container">
+
+
+                @foreach ($horarios as $k => $v)
+                    <div class="col card card-primary card-outline elevation-2">
+                        <div id='title'>
+                            <hr width="100%" size="5" noshade>
+                            <h5 class="centrar-content">{{ $v->descripcion }}</h5>
+                            <hr width="100%" size="5" noshade>
+                        </div>
+                        <div class='container'>
+                            <div class='row'>
                                 <div class="centrar-content container">
                                     <!-- <h6 cla>Nota</h6>-->
                                     <input type="text" value="{{ $v->nota }}" class="dial">
                                 </div>
                             </div>
-                            <div class='col'>
+                            <div class='row'>
                                 <div class="card-columns">
                                     <div class="card">
                                         <div class="card-body">
-                                            <i class="fa fa-calendar" aria-hidden="true"> Fecha de resoluci&oacute;n:</i>
+                                            <i class="fa fa-calendar" aria-hidden="true"> Fecha de
+                                                resoluci&oacute;n:</i>
                                             <p class="card-text">{{ $v->fecha_resol }}</p>
                                         </div>
                                     </div>
                                     <div class="card">
                                         <div class="card-body">
                                             <i class="fa fa-laptop" aria-hidden="true"> Modalidad:</i>
-                                            <p class="card-text">@if ($v->modalidad=='P') Presencial @elseif($v->modalidad=='V') Virtual @endif </p>
+                                            <p class="card-text">@if ($v->modalidad == 'P') Presencial @elseif($v->modalidad=='V') Virtual @endif </p>
                                         </div>
                                     </div>
                                     <div class="card">
                                         <div class="card-body">
-                                            <i class="fa fa-circle-o-notch" aria-hidden="true"> Tiempo de resoluci&oacute;n:</i>
+                                            <i class="fa fa-circle-o-notch" aria-hidden="true"> Tiempo de
+                                                resoluci&oacute;n:</i>
                                             <p class="card-text">{{ $v->minutos }} min.</p>
                                         </div>
                                     </div>
@@ -59,47 +60,48 @@
                                         <div class="card-body">
                                             <i class="fa fa-clock-o" aria-hidden="true"> Estado:</i>
                                             <p class="card-text">
-                                               @switch($v->estado)
-                                                   @case('P')
-                                                       Pendiente
-                                                       @break
-                                                   @case('R')
-                                                       Resuelto
-                                                       @break
-                                                   @default
-                                                       Estado Indefinido
-                                               @endswitch
+                                                @switch($v->estado)
+                                                    @case('P')
+                                                        Pendiente
+                                                    @break
+                                                    @case('R')
+                                                        Resuelto
+                                                    @break
+                                                    @default
+                                                        Estado Indefinido
+                                                @endswitch
                                             </p>
                                         </div>
                                     </div>
-                                   @if ($v->modalidad=='P')
-                                   <div class="card">
-                                        <div class="card-body">
-                                            <i class="fa fa-clock-o" aria-hidden="true"> Aula:</i>
-                                            <p class="card-text">{{ $v->aula }}</p>
+                                    @if ($v->modalidad == 'P')
+                                        <div class="card">
+                                            <div class="card-body">
+                                                <i class="fa fa-clock-o" aria-hidden="true"> Aula:</i>
+                                                <p class="card-text">{{ $v->aula }}</p>
+                                            </div>
                                         </div>
-                                    </div>
-                                   @endif
-                                    
+                                    @endif
+
                                 </div>
                             </div>
+
+
+                            @if ($v->flag_jura == 'N' && $v->fecha_comp <= Carbon\Carbon::now() && $v->estado == 'P')
+                                <form action="{{ route('BeginExamen') }}" method="POST">
+                                    @csrf
+                                    <input type="text" class="d-none" name='id_examen'
+                                        value="{{ $v->id_examen }}">
+                                    <div style="padding-bottom: 20px" class='centrar-content'>
+                                        <button type="submit" class="btn btn-success">Ingresar</button>
+                                    </div>
+                                </form>
+                            @endif
+
                         </div>
-                        
-                       @if ($v->flag_jura=='N' && $v->fecha_comp<=Carbon\Carbon::now() && $v->estado=='P')
-                       <form action="{{ route('BeginExamen') }}" method="POST">
-                        @csrf
-                            <input type="text" class="d-none" name='id_examen' value="{{ $v->id_examen }}"> 
-                            <div style="padding-bottom: 20px" class='centrar-content'>
-                                <button type="submit" class="btn btn-success">Ingresar</button>
-                            </div>
-                        </form>
-                       @endif
-                        
                     </div>
-                </div>
-                <br>
-            @endforeach
-        </div>
+                    <br>
+                @endforeach
+            </div>
         @endif
         <div class='col-1'></div>
     </div>
