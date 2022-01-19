f10d785cf0bedb1b1419a70199a0598edbf28960
CarlosCortezRubio <presto_ccr@hotmail.com>
2022-01-19 01:05:17 -0500

    --

--- resources/views/inscripcion/Examen/index.blade.php
+++ resources/views/inscripcion/Examen/index.blade.php
@@ -102,7 +102,7 @@
                 $.ajax({
                     type: "GET",
                     url: "{{ route('CargarAudio') }}",
-                    data: {'archivo':archivo.prototype.toString}, 
+                    data: {'archivo':archivo}, 
                     success: function(data){
                         alert(data);
                         if (data) {
