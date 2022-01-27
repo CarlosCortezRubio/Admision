c3458c2bee2c435715c4b85f09711adcf6dc0fc5
CarlosCortezRubio <presto_ccr@hotmail.com>
2022-01-26 21:26:21 -0500

    --

--- resources/views/inscripcion/ficha/horario.blade.php
+++ resources/views/inscripcion/ficha/horario.blade.php
@@ -13,7 +13,8 @@
 
 
                 @foreach ($horarios as $k => $v)
-                    <div class="col-6 card card-primary card-outline elevation-2">
+                    <div class="col-1"></div>
+                    <div class="col-5 card card-primary card-outline elevation-2">
                         <div id='title'>
                             <hr width="100%" size="5" noshade>
                             <h5 class="centrar-content">{{ $v->descripcion }}</h5>
@@ -99,7 +100,7 @@
 
                         </div>
                     </div>
-                    <br>
+                    <div class="col-1"></div>
                 @endforeach
           
         @endif
