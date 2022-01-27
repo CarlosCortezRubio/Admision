8943d3a2c4c851521f09eb78f321f09b7978a483
CarlosCortezRubio <presto_ccr@hotmail.com>
2022-01-26 23:36:01 -0500

    --

--- app/Http/Controllers/FichaController.php
+++ app/Http/Controllers/FichaController.php
@@ -115,7 +115,7 @@ class FichaController extends Controller
                      ->join('admision.adm_aula as au','pe.id_aula','au.id_aula')
                      ->join('admision.adm_examen as ex','pe.id_examen','ex.id_examen')
                      ->join('admision.adm_examen_admision as exd','ex.id_examen','exd.id_examen')
-                     ->leftJoin('admision.adm_programacion_examen as repost','repost.id_programacion_examen','pe.id_prog_requ')
+                     ->leftJoin('admision.adm_postulante as repost','repost.id_programacion_examen','pe.id_prog_requ')
                      ->where(function ($query) {
                         $query->whereNull('pe.id_prog_requ')
                               ->orWhere('repost.nume_docu_sol', Auth::user()->ndocumento);
